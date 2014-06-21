c2b52dcf7da020ad9a45a8f2042ce6d6fa7bb947
Lucas Possatti <lucas_possatti@hotmail.com>
2014-06-21 03:16:39 -0300

    Informações para o log
    O aplicativo agora "loga" as seguintes informações:
     - de onde está vindo uma nova conexão,
     - quem deixou a sala,
     - e as mensagens que estão sendo trocadas.

--- index.js
+++ index.js
@@ -5,17 +5,35 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 
+// Log the data recieved from parameter
+function log(logmessage) {
+	console.log(logmessage);
+}
+
 server.listen(port, function () {
-	console.log('Server listening at port %d', port);
+	log('Server listening at port %d', port);
 });
 
 // Routing
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function (socket) {
+	// get the user address and log it
+	var address = socket.handshake.address;
+    log("Nobody joined from " + address.address + ":" + address.port);
+
 	// when the client emits 'new message', this listens and executes
 	socket.on('new message', function (message) {
+		// log the message
+		log("Nobody sad " + message);
+
 		// we tell the clients to execute 'new message'
 		io.emit('new message', message);
+	});
+
+	// when the client disconnects
+	socket.on('disconnect', function () {
+		// log who is leaving
+		log("Nobody left from " + address.address + ":" + address.port);
 	});
 });
